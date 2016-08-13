05985d246da1f80a8d3d592fcedbaf87252f72fb
rdemirr <rdemir057@gmail.com>
2016-08-13 13:10:46 +0300

    Tickets_add silindi , ticket ekleme işlemi pop-up tarafına geçirildi.  User ekleme işlemi başarılı olduğunda jqrid'i otomatik güncelleme işlemi yapıldı.

--- WebContent/js/user.js
+++ WebContent/js/user.js
@@ -11,19 +11,19 @@ function getAllUsers() {
 		}, {
 			label : "E-Mail",
 			name : 'email',
-			width : 100
+			width : 120
 		}, {
 			label : "Password",
 			name : 'password',
-			width : 100
+			width : 60
 		}, {
 			label : "CompanyID",
 			name : 'companyID',
-			width : 100
+			width : 60
 		}, ],
 		viewrecords : true,
-		height : 150,
-		width : 780,
+		height : 550,
+		width : 750,
 		styleUI : 'Bootstrap',
 		rowNum : 10,
 		pager : "#jqGridPager"
@@ -60,7 +60,7 @@ function addUser() {
 	}
 
 	if (newRoles.length == 0 || newName == "" || newEmail == ""
-	 							|| newCompany == "" || newPassword == "") {
+			|| newCompany == "" || newPassword == "") {
 		console.log("error: fill all boxes");
 		$("#modalAddUserMessage").html("Please fill all boxes");
 	} else {
@@ -74,28 +74,37 @@ function addUser() {
 		};
 
 		$.ajax({
-			type : "POST",
-			url : '/Ticket_System/rest/user/addUser',
-			contentType : "application/json",
-			mimeType : "application/json",
-			data : JSON.stringify(person),
-			success : function(status) {
-				if(status=="SUCCESS"){
-					$("#modalAddUserMessage").text("User added. Close Window.");
-				}else if(status=="USER_EXIST"){
-					$("#modalAddUserMessage").text("User exist!! Email must be unique.");
-				}else{
-					$("#modalAddUserMessage").text("Error occured!. Open a ticket.");
-				}
-			},
+					type : "POST",
+					url : '/Ticket_System/rest/user/addUser',
+					contentType : "application/json",
+					mimeType : "application/json",
+					data : JSON.stringify(person),
+					success : function(status) {
+						if (status == "SUCCESS") {
+							$("#modalAddUserMessage").text(
+									"User added. Close Window.");
 
-			error : function() {
-				alert("User cannot added please try again. ");
-			}
-		});
+							// Kullancı başarıyla eklendiğinde gridi refresh
+							// ediyorum.
+							$('#jqGrid').trigger('reloadGrid');
+
+							console.log("grid reloadeded")
+						} else if (status == "USER_EXIST") {
+							$("#modalAddUserMessage").text(
+									"User exist!! Email must be unique.");
+						} else {
+							$("#modalAddUserMessage").text(
+									"Error occured!. Open a ticket.");
+						}
+					},
+
+					error : function() {
+						alert("User cannot added please try again. ");
+					}
+				});
 	}
 }
 
-$(document).ready(function(){
+$(document).ready(function() {
 	getAllUsers();
 });
