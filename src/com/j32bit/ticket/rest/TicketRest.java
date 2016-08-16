2cea1f9a7b607482a7f17cd6e1c2c0c7e1687344
rdemirr <rdemir057@gmail.com>
2016-08-16 20:52:07 +0300

    TicketRest changed

--- src/com/j32bit/ticket/rest/TicketRest.java
+++ src/com/j32bit/ticket/rest/TicketRest.java
@@ -8,12 +8,17 @@ import javax.ws.rs.Consumes;
 import javax.ws.rs.GET;
 import javax.ws.rs.POST;
 import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
+import javax.xml.bind.annotation.XmlAccessType;
+import javax.xml.bind.annotation.XmlAccessorType;
 
 import com.j32bit.ticket.bean.Ticket;
 import com.j32bit.ticket.service.ServiceFacade;
 
 @Path("/ticket")
+@XmlAccessorType(XmlAccessType.FIELD)
+@Produces({ MediaType.APPLICATION_JSON, MediaType.APPLICATION_XML ,MediaType.TEXT_PLAIN})
 public class TicketRest {
 
 	@Path("/addTicket")
@@ -27,6 +32,7 @@ public class TicketRest {
 	
 	@Path("/getAllTickets")
 	@GET
+	@Produces(MediaType.APPLICATION_JSON)
 	@RolesAllowed("admin")
 	public ArrayList<Ticket> getAllTickets() {
 		return  ServiceFacade.getInstance().getAllTickets();
